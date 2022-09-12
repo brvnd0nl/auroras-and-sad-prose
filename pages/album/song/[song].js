2568f19757b307f2d86c0b62374b5bbe966393fa
Brandon Marchena <brandon.marchena@aplinet.co>
2022-09-12 00:31:01 -0500

    Correccion en consulta de lyrics y pagina de error

--- pages/album/song/[song].js
+++ pages/album/song/[song].js
@@ -25,10 +25,7 @@ const Song = ({ lyrics }) => {
 export const getServerSideProps = async (ctx) => {
   const { artist, song } = ctx.query;
 
-  let nSong = song.substring(
-    0,
-    song.indexOf("(") - 1
-  );
+  const nSong = song.includes('(') ? song.substring(0, song.indexOf("(") - 1) : song;
 
   if (!artist || !song) {
     return { redirect: { destination: "/", permanent: false } };
@@ -56,7 +53,7 @@ export const getServerSideProps = async (ctx) => {
   // const lyrics = parseSongHTML(htmlResponse);
   //-------------------------------------------------------
   // const uid = '10817';
-  // const token = 'qDMDkCMNOobSnc04';  
+  // const token = 'qDMDkCMNOobSnc04';
 
   // const htmlResponse = await fetch(
   //   `https://www.stands4.com/services/v2/lyrics.php?uid=${uid}&tokenid=${token}&artist=${artist
@@ -72,12 +69,14 @@ export const getServerSideProps = async (ctx) => {
 
   const searches = await Client.songs.search(`${nSong} ${artist}`);
 
-  const songData = searches.find(item => item.title === nSong && item.artist.name === artist);
+  const songData = searches.find(
+    (item) => item.title === nSong && item.artist.name === artist
+  );
 
-  if(!songData){
+  if (!songData) {
     return {
       notFound: true,
-      redirect: { destination: "/", permanent: false }
+      redirect: { destination: "/", permanent: false },
     };
   }
 
@@ -87,7 +86,8 @@ export const getServerSideProps = async (ctx) => {
     props: {
       lyrics,
       // html: htmlResponse
-      // data: songData
+      data: songData,
+      allData: searches,
     },
   };
 };
